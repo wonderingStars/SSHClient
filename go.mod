9906a32cd0171cf4813b0745cf7daa49571e4067
steve <steven.fairclough@icloud.com>
2023-05-13 14:29:36 -0700

    super simple SSH client

---
+++ go.mod
@@ -0,0 +1,20 @@
+module SSHClient
+
+go 1.18
+
+require (
+	github.com/creack/pty v1.1.18
+	github.com/gliderlabs/ssh v0.3.5
+	github.com/pkg/sftp v1.13.5
+	github.com/stretchr/testify v1.8.1
+	golang.org/x/crypto v0.9.0 // indirect
+)
+
+require (
+	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"golang.org/x/crypto/ssh"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+)
+
+const (
+	key           = "AKIAZIGQD7TRCDC44ICG"
+	publicIPNPort = "192.168.0.1:22"
+)
+
+func SendCommand(in io.WriteCloser, cmd string) error {
+	if _, err := in.Write([]byte(cmd + "\n")); err != nil {
+		return err
+	}
+
+	return nil
+}
+func main() {
+
+	pemBytes, err := ioutil.ReadFile(key)
+	if err != nil {
+		log.Fatal(err)
+	}
+	signer, err := ssh.ParsePrivateKey(pemBytes)
+	if err != nil {
+		log.Fatalf("parse key failed:%v", err)
+	}
+	config := &ssh.ClientConfig{
+		Timeout:         time.Second * 5,
+		User:            "ubuntu",
+		Auth:            []ssh.AuthMethod{ssh.PublicKeys(signer)},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	// Connect to the client
+	client, err := ssh.Dial("tcp", publicIPNPort, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	// Create a session
+	session, err := client.NewSession()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
+
+	// Setup StdinPipe to send commands
+	stdin, err := session.StdinPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stdin.Close()
+
+	// Route session Stdout/Stderr to system Stdout/Stderr
+	session.Stdout = os.Stdout
+	session.Stderr = os.Stderr
+
+	// Start a shell
+	if err := session.Shell(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Run configuration commands
+	SendCommand(stdin, "docker run -p 9987:9987/udp -p 10011:10011 -p 30033:30033 -e TS3SERVER_LICENSE=accept teamspeak ")
+	//SendCommand(stdin, "ls")
+	//SendCommand(stdin, "ls")
+
+	session.Wait()
+
+}
